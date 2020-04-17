9ae0b5d5d8d10508c9513d7a5434a1387bde6d7e
Denisko185 <55317671+Denisko185@users.noreply.github.com>
2020-04-17 02:13:42 +0200

    Revert "commit"
    This reverts commit 0b90500d5d2cf458b02310485ef2e97e1bd02b7f, reversing
    changes made to 120f4f6fb0d99345fd35a1af28dd233e062a44df.

--- Signalify/app/src/main/java/com/example/signalify/MainActivity.java
+++ Signalify/app/src/main/java/com/example/signalify/MainActivity.java
@@ -32,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         IMapController mapController;
         ItemizedOverlayWithFocus<OverlayItem> mMyLocationOverlay;
 
--- Signalify/app/src/main/java/com/example/signalify/SplashScreenActivity.java
+++ Signalify/app/src/main/java/com/example/signalify/SplashScreenActivity.java
@@ -23,7 +23,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(SplashScreenActivity.this, ShowDetailActivity.class);
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
                 startActivity(i);
                 finish();
 
