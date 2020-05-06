f849996a74e4d9323c23ea5e2294333f3e9316c2
Lemuel-DONTO <55319038+Lemouz-NALMOK@users.noreply.github.com>
2020-05-06 15:38:57 +0200

    Pull Armand Push

---
+++ Signalify/app/src/main/java/com/example/signalify/databaseAccess/AccessAccidents.java
@@ -0,0 +1,8 @@
+package com.example.signalify.databaseAccess;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class AccessAccidents {
+    private FirebaseDatabase accidentsDatabase;
+    private
+}
